89754594c17015fefc8fd0b75b74ba98e79b2280
Euphoria <hh@Euphoria.cn>
2023-01-06 19:53:58 +0800

    编码解决商品查询的缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -45,12 +44,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-
+        // 判断命中的是否为空值
+        if (shopJson != null){
+            //返回一个错误信息
+            return Result.fail("店铺信息不存在");
+        }
         // 4.不存在，根据id查询数据库
         Shop shop = getById(id);
 
         // 5.不存在，返回错误
         if (shop == null) {
+            // 将空值写入redis
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回错误信息
             return Result.fail("店铺不存在");
         }
 
