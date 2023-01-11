3c3a996b3eac33ecfec0849347c46be3d8ad254b
Euphoria <hh@Euphoria.cn>
2023-01-11 20:19:23 +0800

    附近店铺

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -17,4 +17,7 @@ public interface IShopService extends IService<Shop> {
     Result queryById(Long id);
 
     Result update(Shop shop);
+
+
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
 }
